53e97fbbbcf839ff69726d9332f8d9d6ca2e7b2d
Steven Zhou <zhy29563@hotmail.com>
2026-05-24 16:43:24 +0800

    添加APK编译后自动重命名和复制机制

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -51,5 +51,38 @@ flutter {
     source = "../.."
 }
 
+// === APK 编译后自动重命名并复制到 flutter-apk/ 目录 ===
+android.applicationVariants.configureEach {
+    outputs.configureEach {
+        val appName = "Insurer"
+        val version = versionName
+        val buildType = buildType.name
+        (this as com.android.build.gradle.internal.api.BaseVariantOutputImpl).outputFileName = "$appName-$version-$buildType.apk"
+    }
+}
+
+tasks.register("copyReleaseApk") {
+    group = "build"
+    description = "Copy release APK to flutter-apk directory"
+    dependsOn("assembleRelease")
+    doLast {
+        val appName = "Insurer"
+        val version = android.defaultConfig.versionName
+        val srcDir = "${project.buildDir}/outputs/apk/release/"
+        val dstDir = "${project.buildDir}/outputs/flutter-apk/"
+        val srcName = "$appName-$version-release.apk"
+        val srcFile = file("$srcDir$srcName")
+        if (srcFile.exists()) {
+            copy {
+                from(srcFile)
+                into(dstDir)
+            }
+            println("APK copied: $dstDir$srcName")
+        } else {
+            println("APK not found: $srcDir$srcName")
+        }
+    }
+}
+
 dependencies {
 }
